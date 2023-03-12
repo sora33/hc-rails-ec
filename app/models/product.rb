ba424b003f92c5a178763e0e5ec80dd22dd567a8
hiranuma <shuuuya0616@gmail.com>
2023-03-13 08:06:10 +0900

    ⚡️ 画像表示時に画像変形ロジックを追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,5 +5,12 @@ class Product < ApplicationRecord
   validates :price, presence: true, numericality: true
   validates :description, presence: true
 
-  has_many_attached :images
+  has_one_attached :image
+
+  def image_as_thumbnails
+    image.variant(resize_to_fill: [450,300]).processed
+  end
+  def image_as_thumbnail
+    image.variant(resize_to_fill: [600,700]).processed
+  end
 end
