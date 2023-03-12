f359269d2b52ecbe7c4a3386bcd49ce56625f4c7
hiranuma <shuuuya0616@gmail.com>
2023-03-13 08:39:12 +0900

    ♻️ rubocop:disableの対象を追加

--- db/migrate/20230209122759_create_active_storage_tables.active_storage.rb
+++ db/migrate/20230209122759_create_active_storage_tables.active_storage.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # This migration comes from active_storage (originally 20170806125915)
-# rubocop:disable
+# rubocop:disable Metrics/MethodLength, Metrics/AbcSize
 
 class CreateActiveStorageTables < ActiveRecord::Migration[5.2]
   def change
@@ -25,9 +25,7 @@ class CreateActiveStorageTables < ActiveRecord::Migration[5.2]
 
       t.index [:key], unique: true
     end
-  end
 
-  def change
     create_table :active_storage_attachments, id: primary_key_type do |t|
       t.string     :name,     null: false
       t.references :record,   null: false, polymorphic: true, index: false, type: foreign_key_type
@@ -43,9 +41,7 @@ class CreateActiveStorageTables < ActiveRecord::Migration[5.2]
                                                       unique: true
       t.foreign_key :active_storage_blobs, column: :blob_id
     end
-  end
 
-  def change
     create_table :active_storage_variant_records, id: primary_key_type do |t|
       t.belongs_to :blob, null: false, index: false, type: foreign_key_type
       t.string :variation_digest, null: false
@@ -66,4 +62,4 @@ class CreateActiveStorageTables < ActiveRecord::Migration[5.2]
   end
 end
 
-# rubocop:enable
+# rubocop:enable Metrics/MethodLength, Metrics/AbcSize
