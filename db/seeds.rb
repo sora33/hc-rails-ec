f96df29aa5a07773074db934e81c69f32057b42f
hiranuma <shuuuya0616@gmail.com>
2023-04-25 13:24:08 +0900

    画像データも生成されるように変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,12 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 10.times do |n|
-  product_a = Product.new(
+  product = Product.create!(
     name: "テスト商品名#{n + 1}",
     description: 'とってもおいしいです',
     price: rand(500..1500)
   )
   # product_a.image.attach(io: File.open(Rails.root.join('app/assets/images/sora.jpg')), filename: 'sora.jpg')
-  product_a.save!
+  image_path = Rails.root.join("app/assets/images/sora.jpg")
+  product.image.attach(io: File.open(image_path), filename: "sora.jpg", content_type: 'image/jpg')
 end
