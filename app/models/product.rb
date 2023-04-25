ca3352338426a5ec9657be28e305c0d64b527786
hiranuma <shuuuya0616@gmail.com>
2023-04-25 13:52:43 +0900

    画像のエラーハンドリング

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,10 +8,32 @@ class Product < ApplicationRecord
   has_one_attached :image
 
   def image_as_thumbnails
-    image.variant(resize_to_fill: [450, 300]).processed
+    if image.attached?
+      begin
+        image.variant(resize_to_fill: [450, 300]).processed
+      rescue ActiveStorage::FileNotFoundError => e
+        Rails.logger.error "Error loading image: #{e}"
+        # デフォルト画像を返す
+        return "https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
+      end
+    else
+      # 画像が添付されていない場合はデフォルト画像を返す
+      return "https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
+    end
   end
 
   def image_as_thumbnail
-    image.variant(resize_to_fill: [600, 700]).processed
+    if image.attached?
+      begin
+        image.variant(resize_to_fill: [600, 700]).processed
+      rescue ActiveStorage::FileNotFoundError => e
+        Rails.logger.error "Error loading image: #{e}"
+        # デフォルト画像を返す
+        return "https://dummyimage.com/600x700/dee2e6/6c757d.jpg"
+      end
+    else
+      # 画像が添付されていない場合はデフォルト画像を返す
+      return "https://dummyimage.com/600x700/dee2e6/6c757d.jpg"
+    end
   end
 end
