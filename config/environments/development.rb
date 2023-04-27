0c460637205b0726d88f2d36c420e4166c5af3d6
hiranuma <shuuuya0616@gmail.com>
2023-04-27 09:18:17 +0900

    rubocopを実行

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,7 +46,6 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost:3333' }
   config.action_mailer.delivery_method = :letter_opener_web
 
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
