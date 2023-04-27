ba21efc2b5542af8fce85d1632e0619f73b331b7
hiranuma <shuuuya0616@gmail.com>
2023-04-27 16:56:58 +0900

    ordersテーブルのcart_idの外部キー制約を削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_25_132930) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_27_075048) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -86,5 +86,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_25_132930) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "cart_items", "carts"
   add_foreign_key "cart_items", "products"
-  add_foreign_key "orders", "carts"
 end
