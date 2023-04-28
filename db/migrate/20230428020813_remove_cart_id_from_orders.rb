33cf2fe5c6b093e02a4c3461be3d61a6d40fbfb6
hiranuma <shuuuya0616@gmail.com>
2023-04-28 13:17:14 +0900

    マイグレーションの記述を修正

--- db/migrate/20230428020813_remove_cart_id_from_orders.rb
+++ db/migrate/20230428020813_remove_cart_id_from_orders.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class RemoveCartIdFromOrders < ActiveRecord::Migration[7.0]
   def change
-    remove_column :orders, :cart_id
+    remove_column :orders, :cart_id, :bigint
   end
 end
