8560535a33e58507910bd0fe95873ff6c2f7778f
hiranuma <shuuuya0616@gmail.com>
2023-04-28 13:17:02 +0900

    関数を分割するリファクタリング

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,27 +9,13 @@ class OrdersController < ApplicationController
 
   def create
     @order = Order.new(order_params)
-    Order.transaction do  # start トランザクション処理
+    Order.transaction do # start トランザクション処理
       if @cart.cart_items.present?
-        # カートアイテムをオーダーアイテムに変換
-        @order.order_items = @cart.cart_items.map do |cart_item|
-          OrderItem.new(
-            product_name: cart_item.product.name,
-            price: cart_item.product.price,
-            quantity: cart_item.quantity
-          )
-        end
-        @order.save!
-        # カートをクリアして、新規作成
-        session[:cart_id] = nil
-        set_cart
-        # メール送信
-        OrderMailer.order_confirmation(@order).deliver_now
-        redirect_to root_path, notice: 'ご購入ありがとうございました。'
+        create_order_and_send_confirmation(@order)
       else
         redirect_to cart_path, notice: '商品をカートに入れてください。'
       end
-    end  # end トランザクション処理
+    end
   rescue ActiveRecord::RecordInvalid => e
     redirect_to cart_path, notice: e.message
   end
@@ -50,5 +36,28 @@ class OrdersController < ApplicationController
     authenticate_or_request_with_http_basic do |username, password|
       username == 'admin' && password == 'pw'
     end
+  end
+
+  # カート情報から注文商品情報（order_items_from_cart）を作成
+  def order_items_from_cart(cart)
+    cart.cart_items.map do |cart_item|
+      OrderItem.new(
+        product_name: cart_item.product.name,
+        price: cart_item.product.price,
+        quantity: cart_item.quantity
+      )
+    end
+  end
+
+  # オーダー作成とメール送信をトランザクション処理にする
+  def create_order_and_send_confirmation(order)
+    Order.transaction do
+      order.order_items = order_items_from_cart(@cart)
+      order.save!
+      session[:cart_id] = nil
+      set_cart
+      OrderMailer.order_confirmation(order).deliver_now
+    end
+    redirect_to root_path, notice: 'ご購入ありがとうございました。'
   end
 end
