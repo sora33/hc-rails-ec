00fa065e76ea6ac2970563ab1861f94d63f98726
hiranuma <shuuuya0616@gmail.com>
2023-05-03 06:50:59 +0900

    処理失敗時のエラーハンドリングを追加

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,12 +5,16 @@ class CartsController < ApplicationController
 
   def apply_promo_code
     promo_code = PromotionCode.find_by(code: params[:code])
-    if promo_code.present? && !promo_code.is_used
-      @cart.promotion_code = promo_code
-      @cart.save
+    if promo_code.blank? || promo_code.is_used
+      redirect_to cart_path, alert: '無効なプロモーションコードです。'
+      return
+    end
+
+    @cart.promotion_code = promo_code
+    if @cart.save
       redirect_to cart_path, notice: 'プロモーションコードが適用されました。'
     else
-      redirect_to cart_path, alert: '無効なプロモーションコードです。'
+      redirect_to cart_path, alert: 'プロモーションコードの適用に失敗しました。再度お試しください。'
     end
   end
 end
